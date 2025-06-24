492d1e31516e7769fb08e1c355354bba9f2b0bb5
Timolio <94037690+Timolio@users.noreply.github.com>
2025-06-25 00:11:18 +0200

    show user email on home page

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,12 @@ export default async function Home() {
     return (
         <div className="flex flex-col justify-center items-center min-h-screen gap-4">
             <h1>Welcome!</h1>
+            {user && (
+                <div className="text-neutral-600">
+                    Logged in as{' '}
+                    <span className="font-semibold">{user.email}</span>
+                </div>
+            )}
             <Link href="/private" className="custom-button">
                 <span>Play</span>
             </Link>
